50fb7c304d2c8c343cb9c95933933a7fc38f9bcd
froxec <durawa.p.soft@gmail.com>
2024-11-23 14:20:59 +0100

    changes to speed up hashing a pandas dataframe

--- src/kedro_cache/framework/hooks/hash.py
+++ src/kedro_cache/framework/hooks/hash.py
@@ -2,7 +2,18 @@ import inspect
 from typing import Any, Callable, Dict
 
 import joblib
+import pandas as pd
 
+def hash_pandas_dataframe(df: pd.DataFrame) -> str:
+        """Hash a pandas DataFrame.
+
+        Args:
+            df: The DataFrame to hash.
+
+        Returns:
+            The hash of the DataFrame.
+        """
+        return pd.util.hash_pandas_object(df, index=True).values
 
 def hash_datadict(d: Dict[str, Any]) -> str:
     """Hash a dictionary of data.
@@ -13,9 +24,17 @@ def hash_datadict(d: Dict[str, Any]) -> str:
     Returns:
         The hash of the dictionary.
     """
-    # calculate hash for each input
-    hash_set = {joblib.hash((k, v), coerce_mmap=True) for k, v in d.items()}
 
+    # calculate hash for each input
+    hash_set = set()
+    for k, v in d.items():
+        if isinstance(v, pd.DataFrame):
+            values = hash_pandas_dataframe(v)
+        else:
+            values = v
+        tuple_hash = joblib.hash((k, values), coerce_mmap=True)
+        hash_set.add(tuple_hash)
+        
     # calculate hash for the set of hashes
     hash = joblib.hash(hash_set)
 
